e8e07f8366ecc27544ee02b7d5ed2d9f6c57419f
Ian L <ileach81@gmail.com>
2022-02-01 14:33:01 -0500

    final commit for issue

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
+  console.log(req.session);
   Post.findAll({
     attributes: [
       "id",
@@ -42,6 +43,10 @@ router.get("/", (req, res) => {
 });
 
 router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
   res.render("login");
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -6,11 +6,24 @@ const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
 app.use(express.static(path.join(__dirname, "public")));
 
 // turn on routes
