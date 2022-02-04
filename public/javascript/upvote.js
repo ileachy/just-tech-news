fd0c4c75a471ffea9c33d6781db5683d8eca1eca
Ian L <ileach81@gmail.com>
2022-02-04 03:00:57 -0500

    logoff

---
+++ public/javascript/upvote.js
@@ -0,0 +1,13 @@
+async function upVoteClickHandler(event) {
+  event.preventDefault();
+
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+
+  console.log("id");
+}
+
+document
+  .querySelector(".upvote-btn")
+  .addEventListener("click", upVoteClickHandler);
